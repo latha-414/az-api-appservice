0d2602340be02cca9a457bdca0761e2a7d5596a1
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-07 11:45:40 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  description = "Azure region"
+  default     = "East US"
+}
+
+variable "environment" {
+  description = "Environment name"
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "Project name"
+  default     = "policytest"
+}

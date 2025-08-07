d6f0cc3c66b897393fd5ab5438c960e29c87a3a2
latha-414 <144301489+latha-414@users.noreply.github.com>
2025-08-07 14:12:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,7 +33,7 @@ resource "azurerm_service_plan" "main" {
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
   
   tags = {
     Environment = "dev"
